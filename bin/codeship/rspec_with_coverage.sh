9f388068a3562c13bffbaa51f16804770ccc3b78
Michael Granados <michael.granados@truckpad.com.br>
2016-11-21 19:07:37 -0200

    better find

--- bin/codeship/rspec_with_coverage.sh
+++ bin/codeship/rspec_with_coverage.sh
@@ -7,7 +7,7 @@ rm -rf coverage
 declare BUNDLE='./bin/bundle exec'
 declare RSPEC="${BUNDLE} rspec"
 
-for PATH_NAME in `find spec -type d -depth 1`
+for PATH_NAME in `find spec -maxdepth 1 -type d | grep -v '^spec$'`
 do
     COVERAGE_PATH="${PWD}/coverage/$(basename $PATH_NAME)" $RSPEC $PATH_NAME
 done
