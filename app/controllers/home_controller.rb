495c19220e8950e8790090dd7b8af5d4520504bc
Michael Granados <michael.granados@truckpad.com.br>
2016-11-22 10:24:07 -0200

    some new specs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,9 @@ class HomeController < ApplicationController
     if 'Michael' == params[:name]
       @name = 'Michael'
     end
+    @name = 'Vegeta' if 'Vegeta' == params[:name]
+    @name = 'Goku' if 'Goku' == params[:name]
+    @name = 'Bulma' if 'Bulma' == params[:name]
     render plain: @name
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -9,7 +9,11 @@ RSpec.describe HomeController, type: :controller do
 
   it 'must get index page with Michael' do
     get :index, params: { name: 'Michael' }
-    expect(response).to be_success
     expect(assigns(:name)).to eq('Michael')
+  end
+
+  it 'must get index page with Bulma' do
+    get :index, params: { name: 'Bulma' }
+    expect(assigns(:name)).to eq('Bulma')
   end
 end
